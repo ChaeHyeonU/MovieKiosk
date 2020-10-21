34f1199af9dce744729171069dc3a8258f675c99
Jtility37 <leejewon97@gmail.com>
2020-10-21 21:13:43 +0900

    회원가입 시 중복처리 및 신규 회원가입 거의 완료
    1. 아직 문법규칙의 경우 정돈되지 않음
    2. 아이디가 중복이면 신규가입을 받지 않는 의미규칙(existID())은 구현완료
    3. 로그인은 회원가입에 비해 간단하여 아직 미구현
    4. 거의 완료라고 한 이유는 아직 파일이 존재하지 않거나 데이터가 없을 시 오류
    5. 때문에 예시 데이터파일도 추가

--- LogIn.java
+++ LogIn.java
@@ -1,10 +1,9 @@
-import java.io.IOException;
+import java.io.*;
 import java.util.Scanner;
 
 public class LogIn {
 //    고쳐야 할 것
-//    1. IntelliJ 에서의 실행이 shift + f10 이 아니라, alt + shift + f10과 (Main class를 선택할)엔터
-//    2. 7.1 "입력:" 한글임ㅠ
+//    1. IntelliJ 에서의 실행이 shift + f10 이 아니라, alt + shift + f10과 (실행할 main 함수가 포함된 class를 선택 후)엔터
 
     static Scanner scan = new Scanner(System.in);
     public static void main(String[] args) {
@@ -14,7 +13,7 @@ public class LogIn {
 //        1. Log in 2. Sign up 3. Exit
 //        입력:
         System.out.println("1. Log in 2. Sign up 3. Exit");
-        System.out.print("입력:");
+        System.out.print("입력: ");
         String order = scan.nextLine();
         switch (order){
             case "1":
@@ -35,15 +34,13 @@ public class LogIn {
     }
 
     private static void signUp() {
-//        물어볼 것
-//        1. a와 b로 이루어진 문자열이면, 둘 다 꼭 필수로 들어가야 한다는 뜻인가
-//        2. "input:" 뒤에 공백이 있고 없고 까지 정확히 구분하여 기획서와 똑같이 만들어야 하는가
+//        ID, PW 둘 다 문법규칙 세부적 조정할 것
         System.out.println("2. Sign up"); // 지울 것
         String ID = signUpID(0);
         String PW = signUpPW(0);
         System.out.println("ID : " + ID + ", PW : " + PW); // 지울 것
-
-
+        // 데이터 파일에 회원정보 등록
+        register(ID, PW);
 
         System.out.print("Registration is complete. Press Enter to go to the login.");
         try {
@@ -54,14 +51,38 @@ public class LogIn {
         inputOrder();
     }
 
-    private static String signUpID(int errorCode) {
+    private static void register(String ID, String PW) {
+        // 아직 파일이 존재하지 않을 시 오류
+        // 아직 파일에 데이터가 존재하지 않을 시 오류
+        try {
+            String str;
+            // 기존 회원정보 str에 불러오기
+            BufferedInputStream bis = new BufferedInputStream(new FileInputStream("./memberInfo.txt"));
+            byte [] buffer = new byte [bis.available()];
+            while( bis.read(buffer) != -1) {}
+            str = new String(buffer);
+            bis.close();
+            // 새로운 회원정보 str에 추가 후 파일에 입력하기
+            BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream("./memberInfo.txt"));
+            str +="{\n";
+            str += ID + "\n";
+            str += PW + "\n";
+            str += "}\n";
+            bos.write(str.getBytes());
+            bos.close();
+        } catch (IOException e) {
+            e.getStackTrace();
+        }
+    }
+
+    private static String signUpID(int errCode) {
 //        Enter 5~20 letters and numbers
 //        ID:
-        if (errorCode == 0)
+        if (errCode == 0)
             System.out.println("Enter 5~20 letters and numbers");
-        else if (errorCode == 1)
+        else if (errCode == 1)
             System.out.println("Enter only 5~20 letters and numbers");
-        else if (errorCode == 2)
+        else if (errCode == 2)
             System.out.println("Already exists ID");
 
         System.out.print("ID: ");
@@ -76,23 +97,48 @@ public class LogIn {
 //        영문 소문자와 숫자로 이루어진 문자열
         if (ID.length() < 5 || ID.length() > 20) // 문법규칙 아직 길이제한만
             ID = signUpID(1);
-        else if (existID()) // 의미규칙
+        else if (existID(ID)) // 의미규칙
             ID = signUpID(2);
 
         return ID;
     }
 
-    private static boolean existID() {
-        //아직 무조건 안 존재
+    private static boolean existID(String ID) {
+        // 아직 파일이 존재하지 않을 시 오류
+        try {
+            // bis 방식
+//            BufferedInputStream bis = new BufferedInputStream(new FileInputStream("./memberInfo.txt"));
+//            byte [] b = new byte [bis.available()];
+//            while( bis.read(b) != -1) {}
+//            System.out.println(new String(b));
+//            bis.close();
+            // br 방식 (한줄씩)
+            BufferedReader br = new BufferedReader(new FileReader("./memberInfo.txt"));
+            String str;
+            while ((str = br.readLine()) != null){
+                // str에 개행 전까지 받아옴
+//                System.out.println(str);
+                if (str.equals("{")){
+                    String compID = br.readLine();
+                    if (compID.equals(ID)){
+                        System.out.println("existID : " + compID); // 지울 것
+                        return true;
+                    }
+                }
+            }
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return false;
     }
 
-    private static String signUpPW(int errorCode) {
+    private static String signUpPW(int errCode) {
 //        Enter 8~16 letters, uppercase and lowercase letters, numbers and special characters.
 //        PW:
-        if (errorCode == 0)
+        if (errCode == 0)
             System.out.println("Enter 8~16 letters, uppercase and lowercase letters, numbers and special characters.");
-        else if (errorCode == 1)
+        else if (errCode == 1)
             System.out.println("Enter only 8~16 letters, uppercase and lowercase letters, numbers and special characters.");
 
         System.out.print("PW: ");
@@ -102,9 +148,6 @@ public class LogIn {
     }
 
     private static String checkPW(String PW) {
-//        고쳐야 할 것
-//        1. (추가) 영문과 특수문자와 숫자를 포함
-
 //        길이가 8이상 16이하
 //        공백 포함하지 않음
 //        로마자 영문 대/소문자를 구분
@@ -117,7 +160,7 @@ public class LogIn {
 
     private static void logIn() {
         System.out.println("1. Log in"); // 지울 것
-        System.out.print("ID:");
+        System.out.print("ID: ");
         String ID = scan.nextLine();
     }
 }
