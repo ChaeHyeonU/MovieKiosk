7b9e9f48b78e8815d56a547fe67b50966e3fce15
Roh jiwnoo <50816927+best01211@users.noreply.github.com>
2020-10-24 18:26:02 +0900

    Update PriceManager.java

--- PriceManager.java
+++ PriceManager.java
@@ -1,12 +1,18 @@
+package konkuk;
+
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -25,7 +31,6 @@ public class PriceManager {
 	private double totalPrice = 0;            // 최종 가격
 	private String timeinform = null;         // 영화 시작 시간
 	private int timeI; // only 0,1,2          // 선택한 시간대(조조,일반,심야)
-	private String timeStr;                   // 선택한 시간
 	private int DI;    // only 0,1,2          // 선택한 2D,3D,4D
 	private int[][] seatInform;               // int [선택한 좌석 등급][앉는 사람의 나이]  
 	private String[][] seatInformStr;         // String [선택한 좌석][앉는 사람의 나이]        
@@ -67,14 +72,13 @@ public class PriceManager {
 	}
 	
 	private void moiveChange() {
-		this.movieInf = this.movieInform.split(" ");
+		this.movieInf = this.movieInform.split("\r\n");
+		//this.movieInf = this.movieInform.split(" ");
 	}
 	
 	private void timeChange() {   // 조조,일반,심야 구분
 		String[] str = this.movieInf[3].split(":");
-		this.timeStr = str[0];
-		String[] arr = this.timeStr.split(":");
-		int timetemp = Integer.parseInt(arr[0]);
+		int timetemp = Integer.parseInt(str[0]);
 		if(timetemp>4 && timetemp<8) this.timeI = 0;
 		else if(timetemp>=8 && timetemp <24) this.timeI = 1;
 		else this.timeI = 2;
@@ -86,7 +90,7 @@ public class PriceManager {
 		else this.DI = 2;
 	}
 	
-	public String[][] seatMoiveInf(){  //영화정보를 찾고 
+	private String[][] seatMoiveInf(){  //영화정보를 찾고 
 		List<String> seat_list = new ArrayList<String>();
 		try {
 			File seatInf = new File("SeatInf.txt");
@@ -118,7 +122,7 @@ public class PriceManager {
 		return seat_list_detail;
 	}
 	
-	public int findSeat() {
+	private int findSeat() {
 		String[][] seat_list_detail = this.seatMoiveInf();
 		int find=0;
 		for(int i =0;i<seat_list_detail.length;i++) {
@@ -132,22 +136,18 @@ public class PriceManager {
 		return find;
 	}
 
-	public void seatInformChange() {    // 좌석 정보를  priceCalculator에게 편하게 만들어주는 메소드
+	private void seatInformChange() {    // 좌석 정보를  priceCalculator에게 편하게 만들어주는 메소드
 		this.seatInform = new int[this.seatInformStr.length][this.seatInformStr[0].length];
 		for(int i=0;i<this.seatInform.length;i++) this.seatInform[i][1] = Integer.parseInt(this.seatInformStr[i][1]);
 		
 		String[][] seat_list_detail = this.seatMoiveInf();
-		int find = this.findSeat();
-		
 		//등급 나누기
-		String[] seatline = seat_list_detail[find][5].split("\t");
-		int line = seatline.length;
-		int grade = line/3;
+		String[] seatline = seat_list_detail[this.findSeat()][5].split("\t");
+		int grade = seatline.length/3;
 		
 		//등급 처리
 		for(int i=0;i<this.seatInform.length;i++) {
-			char ch = this.seatInformStr[i][0].charAt(0);
-			int temp = (int)ch;
+			int temp = (int)(this.seatInformStr[i][0].charAt(0));
 			if(temp<grade+65) this.seatInform[i][0] = 0;
 			else if(temp<grade*2+65) this.seatInform[i][0] = 1;
 			else this.seatInform[i][0] = 2;
@@ -211,17 +211,62 @@ public class PriceManager {
 	
 	private void ReserveSave() {   // 예매 내역을 유저 정보에 저장 
 		String str = this.movieSort();
+		Reserveupdate();
 		String user = this.UserName;
 		//유저 찾는 함수 필요
 		
-		Reserveupdate();
 	}
 	
 	private void Reserveupdate() { // 예매내역을 통해 좌석 예약을 업데이트
+		String[][] seat_list_detail = this.seatMoiveInf();
+		String[] seatline = seat_list_detail[this.findSeat()][5].split("\t");
+		char[][] seat_detail = new char[seatline.length][seatline[0].toCharArray().length];
+		for(int i=0;i<seat_detail.length;i++) {for(int q=0;q<seat_detail[i].length;q++) seat_detail[i][q] = '0';}
 		
-		//영화 찾는 함수 필요
+		for(int i=0;i<this.seatInformStr.length;i++) {
+			char[] ch = this.seatInformStr[i][0].toCharArray();
+			int A = (int)ch[0] - 65;
+			String number = "";
+			for(int q=1;q<ch.length;q++) number += ch[q];
+			seat_detail[A][Integer.parseInt(number)] = '1';
+		}
+		String str = "";
+		for(int i=0;i<seat_detail.length;i++) {
+			for(int q=0;q<seat_detail[i].length;q++) {
+				str += seat_detail[i][q];
+			}
+			seatline[i] = str;
+			str = "";
+		}
+		for(int i=0;i<seatline.length;i++) str = str + seatline[i] + "\t";
+		//seat_list_detail[this.findSeat()][5] = str;
+		
+		File file = new File("SeatInf.txt");
+		String temp = "";
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
+			
+			String line;
+			for(int i=0;i<this.findSeat()*8+6;i++) {
+				line = br.readLine();
+				temp += (line+ "\r\n");
+			}
+			br.readLine();
+			temp += (str + "\r\n");
+			
+			while((line = br.readLine())!= null) temp += (line + "\r\n");
+			FileWriter fw =new FileWriter("SeatInf.txt");
+			fw.write(temp);
+			fw.close();
+			br.close();
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		
 	}
-	private String movieSort() {
+	private String movieSort() {      //출력하기 위해 영화정보 정렬
 		String str = this.movieInf[4]+",";
 		str = str+ this.movieInf[3]+",";
 		str = str+ this.movieInf[2].charAt(0)+"D,";
@@ -230,7 +275,7 @@ public class PriceManager {
 		return str;
 	}
 	
-	private void pricePrint() { // 결제 화면 출력
+	private void pricePrint() {   // 결제 화면 출력
 		String[] str = movieSort().split(",");
 		for(String a: str) System.out.println(a);
 		System.out.println();
