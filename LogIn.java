ee09c1cb9b9e5eadde84d0c6e39f7efcdd9ff744
Jtility37 <leejewon97@gmail.com>
2020-10-24 20:09:18 +0900

    문법규칙 세분화에 따른 오류처리 구현 완료
    1. 각 오류상황에 맞는 출력문구 생성 및 수정
    2. 길이 말고도, 공백 및 다른 문법규칙(필수문자 포함여부) 구현
    3. 회원가입 시 ID, PW 모두 구현 완료
    4. 로그인만 구현하면 됨
    5. 기획서에 맞게 주석 수정 및 수정사항 삭제

--- LogIn.java
+++ LogIn.java
@@ -1,10 +1,8 @@
 import java.io.*;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class LogIn {
-//    고쳐야 할 것
-//    1. IntelliJ 에서의 실행이 shift + f10 이 아니라, alt + shift + f10과 (실행할 main 함수가 포함된 class를 선택 후)엔터
-
     static Scanner scan = new Scanner(System.in);
     public static void main(String[] args) {
         inputOrder();
@@ -33,7 +31,6 @@ public class LogIn {
     }
 
     private static void signUp() {
-//        ID, PW 둘 다 문법규칙 세부적 조정할 것
         String ID = signUpID(0);
         String PW = signUpPW(0);
         // 데이터 파일에 회원정보 등록
@@ -81,10 +78,18 @@ public class LogIn {
 //        Enter 5~20 letters and numbers
 //        ID:
         if (errCode == 0)
-            System.out.println("Enter 5~20 characters, including at least 1 english lowercase letter and 1 number.");
+            System.out.println("Enter 5~20 characters, only including at least 1 English lowercase letter and 1 number");
         else if (errCode == 1)
-            System.out.println("\"MUST\" 5~20 characters, including at least 1 english lowercase letter and 1 number.");
+            System.out.println("Enter 5~20 characters");
         else if (errCode == 2)
+            System.out.println("Don't type white spaces");
+        else if (errCode == 3)
+            System.out.println("Enter 5~20 characters, including at least 1 English lowercase letter");
+        else if (errCode == 4)
+            System.out.println("Enter 5~20 characters, including at least 1 number");
+        else if (errCode == 5)
+            System.out.println("Enter 5~20 characters, \"MUST ONLY\" including at least 1 English lowercase letter and 1 number");
+        else if (errCode == 6)
             System.out.println("Already exists ID");
 
         System.out.print("ID: ");
@@ -96,24 +101,41 @@ public class LogIn {
     private static String checkID(String ID) {
 //        길이가 5이상 20이하
 //        공백 포함하지 않음
-//        하나 이상의 영문 소문자와 하나 이상의 숫자로 이루어진 문자열
-        if (ID.length() < 5 || ID.length() > 20) // 문법규칙 아직 길이제한만
+//        하나 이상의 영문 소문자와 하나 이상의 숫자로 이루어짐
+        int ch;
+        if (ID.length() < 5 || ID.length() > 20) // 길이
             ID = signUpID(1);
-        else if (existID(ID)) // 의미규칙
+        else if (includeBlank(ID)) // 공백X
             ID = signUpID(2);
+        else if ((ch = checkIDChar(ID)) != 0) // 필수 문자만 포함
+            ID = signUpID(ch);
+        else if (existID(ID)) // 의미규칙
+            ID = signUpID(6);
 
         return ID;
     }
 
+    private static int checkIDChar(String ID) {
+        boolean chSEng = false, chNum = false;
+        int errCode = 0;
+        for (int i = 0; i < ID.length(); i++) {
+            if (Pattern.matches("[a-z]", String.valueOf(ID.charAt(i)))) {
+                chSEng = true;
+            } else if (Pattern.matches("[0-9]", String.valueOf(ID.charAt(i)))) {
+                chNum = true;
+            } else
+                return 5; // 다른게 있음
+        }
+        if (!chSEng)
+            errCode = 3; // 영어소문자
+        else if (!chNum)
+            errCode = 4; // 숫자
+
+        return errCode;
+    }
+
     private static boolean existID(String ID) {
         try {
-            // bis 방식
-//            BufferedInputStream bis = new BufferedInputStream(new FileInputStream("./memberInfo.txt"));
-//            byte [] b = new byte [bis.available()];
-//            while( bis.read(b) != -1) {}
-//            System.out.println(new String(b));
-//            bis.close();
-            // br 방식 (한줄씩)
             BufferedReader br = new BufferedReader(new FileReader("./memberInfo.txt"));
             String str;
             while ((str = br.readLine()) != null){
@@ -137,9 +159,20 @@ public class LogIn {
 //        Enter 8~16 characters, including at least 1 alphabet, 1 number and 1 special character.
 //        PW:
         if (errCode == 0)
-            System.out.println("Enter 8~16 characters, including at least 1 alphabet, 1 number and 1 special character.");
+            System.out.println("Enter 8~16 characters, only including at least 1 alphabet, 1 number and 1 special character(~!@#$%^&*_+=-,./?)");
         else if (errCode == 1)
-            System.out.println("\"MUST\" 8~16 characters, including at least 1 alphabet, 1 number and 1 special character.");
+            System.out.println("Enter 8~16 characters");
+        else if (errCode == 2)
+            System.out.println("Don't type white spaces");
+        else if (errCode == 3)
+            System.out.println("Enter 8~16 characters, including at least 1 alphabet");
+        else if (errCode == 4)
+            System.out.println("Enter 8~16 characters, including at least 1 number");
+        else if (errCode == 5)
+            System.out.println("Enter 8~16 characters, including at least 1 special character(~!@#$%^&*_+=-,./?)");
+        else if (errCode == 6)
+            System.out.println("Enter 8~16 characters, \"MUST ONLY\" including at least 1 alphabet, 1 number and 1 special character(~!@#$%^&*_+=-,./?)");
+
 
         System.out.print("PW: ");
         String PW = scan.nextLine();
@@ -151,11 +184,47 @@ public class LogIn {
 //        길이가 8이상 16이하
 //        공백 포함하지 않음
 //        로마자 영문 대/소문자를 구분
-//        하나이상의 영문자와 하나이상의 숫자와 하나 이상의 특수문자로 이루어진 문자열
-        if (PW.length() < 8 || PW.length() > 16) // 문법규칙 아직 길이제한만
+//        하나이상의 영문자와 하나이상의 숫자와 하나 이상의 특수문자(~!@#$%^&*_+=-,./?)로 이루어짐
+        int ch;
+        if (PW.length() < 8 || PW.length() > 16) // 길이
             PW = signUpPW(1);
+        else if (includeBlank(PW)) // 공백X
+            PW = signUpPW(2);
+        else if ((ch = checkPWChar(PW)) != 0) // 필수 문자만 포함
+            PW = signUpPW(ch);
 
         return PW;
+    }
+
+    private static boolean includeBlank(String str) {
+        for (int i = 0; i < str.length(); i++) {
+            if (Character.isWhitespace(str.charAt(i)))
+                return true;
+        }
+        return false;
+    }
+
+    private static int checkPWChar(String PW) {
+        boolean chEng = false, chNum = false, chSp = false;
+        int errCode = 0;
+        for (int i = 0; i < PW.length(); i++) {
+            if (Pattern.matches("[a-zA-z]", String.valueOf(PW.charAt(i)))) {
+                chEng = true;
+            } else if (Pattern.matches("[0-9]", String.valueOf(PW.charAt(i)))) {
+                chNum = true;
+            } else if (Pattern.matches("[~!@#$%^&*_+=\\-,./?]", String.valueOf(PW.charAt(i)))) {
+                chSp = true;
+            } else
+                return 6; // 다른게 있음
+        }
+        if (!chEng)
+            errCode = 3; // 영어
+        else if (!chNum)
+            errCode = 4; // 숫자
+        else if (!chSp)
+            errCode = 5; // 특수문자
+
+        return errCode;
     }
 
     private static void logIn() {
